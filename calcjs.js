7137a623a7b42ca64e03fee3ebae7b405f9e0c43
aritra-dutta11 <96828090+aritra-dutta11@users.noreply.github.com>
2021-12-29 19:14:35 +0530

    Calculator Simple

---
+++ calcjs.js
@@ -0,0 +1,77 @@
+function getHistory(){
+	return document.getElementById("History-value").innerText;
+}
+function printHistory(num){
+	document.getElementById("History-value").innerText=num;
+}
+function getOutput(){
+	return document.getElementById("Output value").innerText;
+}
+function printOutput(num){
+	if(num==""){
+		document.getElementById("Output value").innerText=num;
+	}
+	else{
+		document.getElementById("Output value").innerText=getFormattedNumber(num);
+	}	
+}
+function getFormattedNumber(num){
+	if(num=="-"){
+		return "";
+	}
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
+}
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+var operator = document.getElementsByClassName("Operator");
+for(var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click',function(){
+		if(this.id=="Clear"){
+			printHistory("");
+			printOutput("");
+		}
+		else if(this.id=="Backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){//if output has a value
+				output= output.substring(0,output.length-1);
+				printOutput(output);
+			}
+		}
+		else{
+			var output=getOutput();
+			var history=getHistory();
+			if(output==""&&history!=""){
+				if(isNaN(history[history.length-1])){
+					history= history.substring(0,history.length-1);
+				}
+			}
+			if(output!="" || history!=""){
+				output= output==""?output:reverseNumberFormat(output);
+				history=history+output;
+				if(this.id=="="){
+					var result=eval(history);
+					printOutput(result);
+					printHistory("");
+				}
+				else{
+					history=history+this.id;
+					printHistory(history);
+					printOutput("");
+				}
+			}
+		}
+	});
+}
+var number = document.getElementsByClassName("Number");
+for(var i =0;i<number.length;i++){
+	number[i].addEventListener('click',function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output!=NaN){ //if output is a number
+			output=output+this.id;
+			printOutput(output);
+		}
+	});
+}
